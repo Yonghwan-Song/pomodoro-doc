be5833af665eefc3a6c690092be041b8447508be
Yonghwan Song <shwanpg@gmail.com>
2025-06-12 14:42:23 +0900

    Replace the excalidraw diagram with a mermaid one

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -104,6 +104,11 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
 };
 
 export default config;
